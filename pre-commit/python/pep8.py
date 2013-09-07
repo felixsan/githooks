833ca1b5262f2d7339e337cacfa4b7c757a09623
Felix Santiago <fsantiago@hearsaycorp.com>
2013-09-07 02:26:00 -0700

    Adding githook for PEP8
    Ignores the following errors by default:
    
    - E501: line too long (> 79 characters)
    - E121: continuation line indentation is not a multiple of four
    - E125: continuation line does not distinguish itself from next logical line
    - E126: continuation line over-indented for hanging indent
    - E128: continuation line under-indented for visual indent

---
+++ pre-commit/python/pep8.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+# Pep 8 githook based on Kevin McConnell's version at http://tech.myemma.com/python-pep8-git-hooks/
+
+import os
+import re
+import shutil
+import subprocess
+import sys
+import tempfile
+
+
+def system(*args, **kwargs):
+    kwargs.setdefault('stdout', subprocess.PIPE)
+    proc = subprocess.Popen(args, **kwargs)
+    out, err = proc.communicate()
+    return out
+
+
+def get_modified_files():
+    modified = re.compile('^(?P<name>.*\.py)$', re.MULTILINE)
+    files = system('git', 'diff', '--staged', '--name-only', '--diff-filter=ACMRTUXB')
+    return modified.findall(files)
+
+
+def main():
+
+    print "* Running Pep8 check..."
+    files = get_modified_files()
+    tempdir = tempfile.mkdtemp()
+    for name in files:
+        filename = os.path.join(tempdir, name)
+        filepath = os.path.dirname(filename)
+        if not os.path.exists(filepath):
+            os.makedirs(filepath)
+        with file(filename, 'w') as f:
+            system('git', 'show', ':%s' % name, stdout=f)
+    output = system('pep8', '--show-source', '--statistics', '--ignore=E501,E121,E125,E126,E128', '.', cwd=tempdir)
+
+    shutil.rmtree(tempdir)
+    if output:
+        print "Not committing since there are Pep8 errors.\n"
+        print output,
+        print
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
